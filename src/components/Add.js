f4b76d0c676c2d51c668287c198bca76112f57c1
Muhammet Türkan <83230346+MuhammetTurkan@users.noreply.github.com>
2022-05-11 11:53:11 +0300

    Add.js file has been added and CSS has ben created

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Add = () => {
-  return <div>Add</div>
+  const [query, setQuery] = useState('')
+
+  function onChange(e) {
+    setQuery(e.target.value)
+  }
+
+  return (
+    <div className="add-page">
+      <div className="container">
+        <div className="add-content">
+          <img src="https://www.themoviedb.org/t/p/w1920_and_h600_multi_faces_filter(duotone,032541,01b4e4)/9ZyAUZrfccsjtDwYgc7yvOBnqM9.jpg"></img>
+          <div className="titles">
+            <h1>Hoş Geldiniz.</h1>
+            <h2>
+              Milyonlarca film, TV şovu ve keşfedilecek kişi. Şimdi keşfedin.
+            </h2>
+          </div>
+          <div className="input-wrapper">
+            <input
+              type="text"
+              placeholder="Film, dizi, kişi ara..."
+              onChange={onChange}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Add
--- src/components/Header.js
+++ src/components/Header.js
@@ -17,7 +17,7 @@ const Header = () => {
               <Link to={'/watched'}>İzlenenler</Link>
             </li>
             <li>
-              <Link to={'/add'}>
+              <Link to={'/add'} className="btn">
                 <i className="fas fa-plus"></i>
               </Link>
             </li>
